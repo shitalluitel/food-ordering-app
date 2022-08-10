3c38f31852ba0797d1bb8a3e8b5e2243bcb1b222
Shital Babu Luitel <ctalluitel@gmail.com>
2022-08-10 17:37:45 +0545

    Implementation of forward ref ( passing ref to components)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,23 @@
+import React, {useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css';
+import CartItemContext from "../../store/CartItemContext";
 
 const HeaderCartButton = (props) => {
-    return (
-        <button className={styles.button} onClick={props.onClick}>
+    const cartItemCtx = useContext(CartItemContext);
+    const numberOfCartItems = cartItemCtx.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount
+    }, 0);
+
+    return (<button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon/>
             </span>
             <span>Your Cart</span>
             <span className={styles.badge}>
-                3
+                {numberOfCartItems}
             </span>
-        </button>
-    )
+        </button>)
 }
 
 export default HeaderCartButton;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,11 @@
+import React from "react";
 import meals from '../../data/dummyMeals';
 import styles from './AvailableMeals.module.css';
 import Card from "../UI/Card";
 import MealItem from "./Items/MealItem";
 
-const AvailableMeals = () => {
-    const mealsList = meals.map(meal => <MealItem key={meal.id} meal={meal}/>)
+const AvailableMeals = (props) => {
+    const mealsList = meals.map(meal => <MealItem key={meal.id} id={meal.id} meal={meal}/>)
 
     return (
         <section className={styles.meals}>
